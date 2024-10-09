35789b2e97d759dba9c6537e47fcd2ef98adaa54
odelola_solomon <odelolasolomon5@gmail.com>
2024-10-09 05:51:30 +0100

    updated the training files and moved on with deployment

--- DataFest/Datafest_sql_script.sql
+++ DataFest/Datafest_sql_script.sql
@@ -1,6 +1,6 @@
 create schema Bestbrain_school
 
-CREATE TABLE Bestbrain_school.Student (
+CREATE TABLE Bestbrain_school.Students (
     Student_ID INT PRIMARY KEY,  -- Unique identifier for each student
     First_Name VARCHAR(50) NOT NULL,  -- Assuming a maximum length of 50 for first name
     Last_Name VARCHAR(50) NOT NULL,  -- Assuming a maximum length of 50 for last name
@@ -8,7 +8,7 @@ CREATE TABLE Bestbrain_school.Student (
     Date_of_Birth DATE NOT NULL,  -- Date of birth field
     Admission_Date DATE NOT NULL,  -- Admission date field
     Class_Section CHAR(4) CHECK (Class_Section IN ('A', 'B', 'C', 'D')) NOT NULL,  -- Class section should be one of A, B, C, D
-    Class_Level VARCHAR(4) CHECK (Class_Level IN ('JSS1', 'JSS2', 'SS1', 'SS2', 'SS3')) NOT NULL,  -- Class level choices
+    Class_Level VARCHAR(4) CHECK (Class_Level IN ( 'SS1', 'SS2', 'SS3')) NOT NULL,  -- Class level choices
     Religion VARCHAR(50),  -- Assuming religion name with a max length of 50 characters
     Tribe VARCHAR(50),  -- Assuming tribe name with a max length of 50 characters
     State_of_Origin VARCHAR(50),  -- Assuming state name with a max length of 50 characters
@@ -40,7 +40,7 @@ CREATE TABLE Bestbrain_school.Teachers (
     Have_Lesson_Note BOOLEAN,
     Salary_NGN DECIMAL(12, 2),
     Teaching_Experience_Years INT,
-    FOREIGN KEY (Student_ID) REFERENCES Bestbrain_school.Student (Student_ID)  -- Reference the students table
+    FOREIGN KEY (Student_ID) REFERENCES Bestbrain_school.Students (Student_ID)  -- Reference the students table
 );
 
 select * from Bestbrain_school.Teachers
@@ -55,7 +55,7 @@ CREATE TABLE Bestbrain_school.Activities (
     Impact_on_Performance VARCHAR(50),  -- Description of how the activity affects performance
     Teacher_Supervisor VARCHAR(100),  -- Name of the teacher supervising the activity
     Parental_Support BOOLEAN,  -- Whether the student receives parental support for the activity
-    FOREIGN KEY (Student_ID) REFERENCES Bestbrain_school.Student (Student_ID)  -- Reference the students table
+    FOREIGN KEY (Student_ID) REFERENCES Bestbrain_school.Students (Student_ID)  -- Reference the students table
 );
 
 select * from Bestbrain_school.Activities
@@ -78,3 +78,21 @@ CREATE TABLE Bestbrain_school.Schooldata (
 
 select * from Bestbrain_school.Schooldata
 
+-- Create Classes Table
+CREATE TABLE Bestbrain_school.class (
+    class_id INT PRIMARY KEY,
+	student_id INT NOT NULL REFERENCES Bestbrain_school.students(Student_ID) ON DELETE CASCADE,
+    class_level VARCHAR(10) NOT NULL,
+    class_section CHAR(2) NOT NULL,
+    teacher_id INT NOT NULL REFERENCES Bestbrain_school.Teachers(Teacher_ID) ON DELETE SET NULL
+   
+);
+
+select * from Bestbrain_school.class
+
+--CREATE TABLE class_students (
+    ---id SERIAL PRIMARY KEY,
+    --class_id INT NOT NULL REFERENCES classes(class_id) ON DELETE CASCADE,
+    --student_id INT NOT NULL REFERENCES students(Student_ID) ON DELETE CASCADE
+--)
+select connection_user, now()
